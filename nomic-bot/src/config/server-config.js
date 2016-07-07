c02454d8db6b3a44852a590da4528f08ab696b8a
Jimmy Miller <jimmy.miller@nextgearcapital.com>
2016-07-07 00:48:55 -0400

    fix server config and update comment about type

--- nomic-bot/src/config/server-config.js
+++ nomic-bot/src/config/server-config.js
@@ -2,7 +2,8 @@
     'use strict';
     module.exports = {
         protocol: 'http',
-        port: 2222,
+         port: 80,
+         host: 'api.nomic.archmageinc.com',
         timezone: 'America/New_York'
     };
 }());
--- nomic-bot/src/universe/big-bang.js
+++ nomic-bot/src/universe/big-bang.js
@@ -2,7 +2,7 @@ import { getPlayerData } from '../utils/github';
 import _ from 'lodash';
 
 // Every one of our commands are going to have the following type signature:
-// Command -> World -> List Action
+// Command -> World -> {world :: World, actions :: List Action}
 // bang is the function that creates our "World"
 
 const findPlayer = (name, players) =>_.find(players, { name })
